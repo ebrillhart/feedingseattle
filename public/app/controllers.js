7c5faef49a1745eacb0692443eef9ad3ba23fd04
ebrillhart <emma.brillhart@gmail.com>
2015-12-18 11:02:04 -0800

    maps

--- public/app/controllers.js
+++ public/app/controllers.js
@@ -7,7 +7,7 @@ angular.module('MealsCtrls', ['MealsServices', 'ngMap', 'ui.bootstrap']).control
     $scope.user = $scope.auth.currentUser();
     // initializing map with NgMap
     // NgMap.getMap().then(function(map) {
-        map.setTimeout(google.maps.event.trigger, 500, map, 'resize');
+        NgMap.setTimeout(google.maps.event.trigger, 500, map, 'resize');
     // });
     // function to be used as callback - filters meals into meal arrays
     var mealType = function(arr) {
